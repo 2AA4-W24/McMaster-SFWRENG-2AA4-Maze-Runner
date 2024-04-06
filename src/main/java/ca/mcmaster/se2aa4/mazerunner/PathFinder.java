1da6466b90b9c69919857a207e5663b7f2e08783
root <root@LAPTOP-SJEDP3BG>
2024-04-06 18:48:32 -0400

    Additional refactoring, adding Position abstraction

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/PathFinder.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/PathFinder.java
@@ -1,7 +1,2 @@
-package ca.mcmaster.se2aa4.mazerunner;
-
-import java.io.IOException;
-
-public interface PathFinder {
-    void findPath(String filename) throws IOException;
+package ca.mcmaster.se2aa4.mazerunner;public interface PathFinder {
 }
