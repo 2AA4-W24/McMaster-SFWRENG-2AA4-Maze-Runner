38242f95f58387b84652ea4d27a93cb81ec14aee
root <root@LAPTOP-SJEDP3BG>
2024-09-09 10:50:16 -0400

    benchmarking refactoring

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Benchmark.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Benchmark.java
@@ -45,39 +45,31 @@ public class Benchmark {
             System.out.println("Maze exploration with baseline " + baseline + " ran for " + (endtime - starttime) + "ms");
         }
 
-        Double baselength = 0.0;
-        Double testlength = 0.0;
-        Character current;
-        for (int i = 0; i < basepath.length(); i++) {
-            current = basepath.charAt(i);
-
-            if (current != ' ') {
-                if (Character.isDigit(current)) {
-                    baselength += Character.getNumericValue(current);
-                    i++;
-                }
-                else {
-                    baselength++;
-                }
-            }
-        }
-        for (int i = 0; i < testpath.length(); i++) {
-            current = testpath.charAt(i);
-
-            if (current != ' ') {
-                if (Character.isDigit(current)) {
-                    testlength += Character.getNumericValue(current);
-                    i++;
-                }
-                else {
-                    testlength++;
-                }
-            }
-        }
+        Double baselength = pathLength(basepath);
+        Double testlength = pathLength(testpath);
         Double speedup = baselength / testlength;
         BigDecimal bd = new BigDecimal(speedup);
         bd = bd.round(new MathContext(2));
         speedup = bd.doubleValue();
         System.out.println("Speedup is " + speedup);
+    }
+
+    private Double pathLength(String path) {
+        Double pathlength = 0.0;
+        Character current;
+        for (int i = 0; i < path.length(); i++) {
+            current = path.charAt(i);
+
+            if (current != ' ') {
+                if (Character.isDigit(current)) {
+                    pathlength += Character.getNumericValue(current);
+                    i++;
+                }
+                else {
+                    pathlength++;
+                }
+            }
+        }
+        return pathlength;
     }
 }
